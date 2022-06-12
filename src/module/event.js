128ba00575bf145efba8f8dcf6f1c8a75b55fbbb
lczCoder <lczcoder@163.com>
2022-06-12 23:55:56 +0800

    feat:监听网页隐藏关闭事件

--- src/module/event.js
+++ src/module/event.js
@@ -1,14 +1,21 @@
 /* 上报事件处理 */
 export default {
   init() {
-    // 关闭网页,上报缓存中的数据
+    // 网页隐藏上报缓存中的数据
     window.document.addEventListener("visibilitychange", () => {
       if (document.visibilityState === "hidden") {
         navigator.sendBeacon(
           "http://localhost:3001/post",
-          JSON.stringify({ name: "lcz" })
+          JSON.stringify({ name: "隐藏" })
         );
       }
     });
+    // 网页销毁,上报缓存中的数据
+    window.onbeforeunload = function(e) {
+      navigator.sendBeacon(
+        "http://localhost:3001/post",
+        JSON.stringify({ name: "关闭" })
+      );
+    }
   },
 };
