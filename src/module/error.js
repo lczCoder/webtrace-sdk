dd32438b81653cbbb836339345811301e87d9cf4
lczCoder <lczcoder@163.com>
2022-06-19 23:24:16 +0800

    feat:重写console.err方法

--- src/module/error.js
+++ src/module/error.js
@@ -1,5 +1,6 @@
 /* 错误处理 */
 import { g_config } from "@m/config";
+import {ErrorJsType} from '@u/declare'
 
 export default {
   init() {
@@ -7,14 +8,52 @@ export default {
     if (g_config.error) {
       // 监控JavaScript错误
       window.addEventListener("error", (event)=>{
-        console.log("error1", event);
-      });
+        console.log(event)
+       
+      },true);
       // 监控未处理的promise.reject事件
       window.addEventListener("unhandledrejection", (event) => {
-        console.log("error2", event.reason);
+        console.log("promise", event.reason);
       });
     } else {
       return false;
     }
   },
+};
+
+function recordJavaScriptError() {
+  // 重写console.error, 可以捕获更全面的报错信息
+  var oldError = console.error;
+  console.error = function () {
+    // arguments的长度为2时，才是error上报的时机
+    // if (arguments.length < 2) return;
+    var errorMsg = arguments[0] && arguments[0].message;
+    var url = WEB_LOCATION;
+    var lineNumber = 0;
+    var columnNumber = 0;
+    var errorObj = arguments[0] && arguments[0].stack;
+    if (!errorObj) errorObj = arguments[0];
+    // 如果onerror重写成功，就无需在这里进行上报了
+    !jsMonitorStarted && siftAndMakeUpMessage(errorMsg, url, lineNumber, columnNumber, errorObj);
+    return oldError.apply(console, arguments);
+  };
+  // 重写 onerror 进行jsError的监听
+  window.onerror = function(errorMsg, url, lineNumber, columnNumber, errorObj)
+  {
+    jsMonitorStarted = true;
+    var errorStack = errorObj ? errorObj.stack : null;
+    siftAndMakeUpMessage(errorMsg, url, lineNumber, columnNumber, errorStack);
+  };
+
+  function siftAndMakeUpMessage(origin_errorMsg, origin_url, origin_lineNumber, origin_columnNumber, origin_errorObj) {
+    var errorMsg = origin_errorMsg ? origin_errorMsg : '';
+    var errorObj = origin_errorObj ? origin_errorObj : '';
+    var errorType = "";
+    if (errorMsg) {
+      var errorStackStr = JSON.stringify(errorObj)
+      errorType = errorStackStr.split(": ")[0].replace('"', "");
+    }
+    var javaScriptErrorInfo = new JavaScriptErrorInfo(JS_ERROR, errorType + ": " + errorMsg, errorObj);
+    javaScriptErrorInfo.handleLogInfo(JS_ERROR, javaScriptErrorInfo);
+  };
 };
--- src/utils/declare.js
+++ src/utils/declare.js
@@ -1,7 +1,19 @@
 // 错误类型状态码
 export const ErrorStatus = {
-  100:'未初始化',
+  100: "未初始化",
   101: "参数类型错误",
   102: "参数缺失",
   103: "重复调用",
-}
+};
+
+// javascript 错误类型
+export const ErrorJsType = new Map([
+  ["EvalError", "Eval函数的错误"],
+  ["RangeError", "范围错误"],
+  ["ReferenceError", "引用错误"],
+  ["SyntaxError", "语法错误"],
+  ["TypeError", "类型错误"],
+  ["URIError", "URI错误"],
+  ["Error", "错误"],
+  ["InternalError", "js引擎内部错误"],
+]);
