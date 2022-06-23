aae10c97de62bf84913663c0d2691f9c7a139b6b
lczCoder <lczcoder@163.com>
2022-06-23 19:25:12 +0800

    feat:性能监控兼容性处理

--- src/module/error.js
+++ src/module/error.js
@@ -23,13 +23,32 @@ export default {
   init() {
     // TODO: 需要考虑 一个错误事件，多次上报的问题。一个按钮点击错误事件，多次上报是没有必要的？？？
     rewriteError();
+    _errorAjaxInit();
+    _errorFetchInit();
+
     // 用户交互行为上报
     // window.addEventListener('click', function(e){
     //   console.log('点击事件',e);
     // })
     // 判断是否自动开启错误上报
     if (g_config.error) {
-      // 监控JavaScript错误
+      // 监控js运行错误
+      window.onerror = function (message, filename, lineno, colno, error) {
+        errorModel(
+          "js",
+          Object.assign(
+            {},
+            {
+              message,
+              filename,
+              lineno,
+              colno,
+              stack: g_config.debug ? error?.stack : null,
+            }
+          )
+        );
+      };
+      // 监控资源加载错误错误
       window.addEventListener(
         "error",
         (event) => {
@@ -47,38 +66,24 @@ export default {
                 }
               )
             );
-          } else {
-            const { message, filename, lineno, colno, error } = event;
-            errorModel(
-              "js",
-              Object.assign(
-                {},
-                {
-                  message,
-                  filename,
-                  lineno,
-                  colno,
-                  stack: g_config.debug ? error?.stack : null,
-                }
-              )
-            );
           }
         },
         true
       );
       // 监控未处理的promise.reject事件
-      window.addEventListener("unhandledrejection", ({ reason }) => {
+      window.addEventListener("unhandledrejection", (e) => {
+        console.log("e", e);
         const source = {};
-        if (reason instanceof Error) {
+        if (e.reason instanceof Error) {
           // 语法错误
-          const { message, stack } = reason;
+          const { message, stack } = e.reason;
           Object.assign(source, {
             message,
             stack: g_config.debug ? stack : null,
           });
         } else {
           Object.assign(source, {
-            reason,
+            reason: e.reason,
           });
         }
         errorModel("promise", source);
@@ -91,4 +96,65 @@ export default {
 
 function rewriteError() {
   // 重写console.error, 可以捕获更全面的报错信息
+}
+
+// fetch的处理
+function _errorFetchInit() {
+  if (!window.fetch) return;
+  let _oldFetch = window.fetch;
+  window.fetch = function () {
+    // console.log('arguments',arguments);
+    // arguments[0] 获取请求接口地址
+    return (
+      _oldFetch
+        .apply(this, arguments)
+        .then((res) => {
+          if (!res.ok) {
+            // 当status不为2XX的时候，上报错误
+            console.log("错误了11", res);
+          }
+          return res;
+        })
+        // 当fetch方法错误时上报
+        .catch((error) => {
+          // error.message,
+          // error.stack
+          // 抛出错误而且上报
+          console.log("错误了22", error);
+        })
+    );
+  };
+}
+
+function _errorAjaxInit() {
+  let ajaxUrl = "";
+  let protocol = window.location.protocol;
+  if (protocol === "file:") return;
+  // 处理XMLHttpRequest
+  if (!window.XMLHttpRequest) {
+    return;
+  }
+  let xmlhttp = window.XMLHttpRequest;
+  let _oldSend = xmlhttp.prototype.send;
+  let _oldOpen = xmlhttp.prototype.open;
+  let _handleEvent = function (event,url) {
+    try {
+      if (event && event.currentTarget && event.currentTarget.status !== 200) {
+        console.log("xhr错误", event);
+        // 在此上报错误
+      }
+    } catch (e) {
+      console.log("Tool's error: " + e);
+    }
+  };
+  xmlhttp.prototype.open = function () {
+    ajaxUrl = arguments[1];
+    return _oldOpen.apply(this, arguments);
+  };
+  xmlhttp.prototype.send = function (url=ajaxUrl) {
+    this.addEventListener("error", (e)=>_handleEvent(e,url)); // 失败
+    this.addEventListener("load", (e)=>_handleEvent(e,url)); // 完成
+    this.addEventListener("abort", (e)=>_handleEvent(e,url)); // 取消
+    return _oldSend.apply(this, arguments);
+  };
 }
--- src/module/performance.js
+++ src/module/performance.js
@@ -4,29 +4,78 @@ import { g_config } from "@m/config";
 export default {
   init() {
     if (g_config.performance) {
-      console.log("性能监控功能已启用");
-
-      const observer = new PerformanceObserver((list) => {
-        list.getEntries().forEach((entry) => {
-          console.table(entry);
-          // if (entry.entryType == "navigation") {
-          // }
-        });
-      });
-      // 观察的类型
-      observer.observe({
-        entryTypes: ["navigation","resource"],
-        /**
-          frame：event-loop 时的每一帧
-          navigation：导航
-          resource：资源
-          mark: 打点，得到一个时间戳
-          measure：在两个点之间测量
-          paint：绘制
-          longtask(好像只有 chrome支持)：任何在浏览器中执行超过 50 ms 的任务，都是 long task
-         */
-      });
-      // console.log(window.performance)
+      const result = compatibleDeal();
+      if (!result) {
+        console.warn("当前平台不支持<performance>相关的api");
+      }
+      // window.addEventListener("load", function () {
+      //   if (sessionStorage.getItem("FIRST_LOAD")!=='1') {
+      //     console.log("window加载完成");
+      //     const t = window.performance.timing;
+      //     console.log(`
+      //     请求响应耗时:${t.responseStart - t.requestStart}
+      //     DOM 解析耗时：${t.domInteractive - t.responseEnd}
+      //     资源加载耗时:${t.loadEventStart - t.domContentLoadedEventEnd}
+      //     首次渲染耗时:${t.responseEnd - t.fetchStart}
+      //     首次可交互耗时:${t.domInteractive - t.fetchStart}
+      //   `);
+      //     sessionStorage.setItem("FIRST_LOAD", "1");
+      //   }
+      // });
     }
   },
+};
+
+/**
+ * @desc 方法兼容判断
+ * @return bool 是否兼容
+ */
+const compatibleDeal = () => {
+  const per = window.PerformanceObserver || null;
+  // 判断window.PerformanceObserver api是否可用
+  if (per) {
+    createPerformance();
+    return true;
+  }
+  // 判断performance api是否可用
+  if (window.performance) {
+    firstLoad();
+    return true;
+  }
+  return false;
+};
+
+// 创建PerformanceObserver观察者 监听资源加载
+const createPerformance = () => {
+  // 观察者，监听Performance变化
+  const observer = new PerformanceObserver((list) => {
+    list.getEntries().forEach((info) => {
+      console.table(info);
+      // 首次加载
+      if (info.entryType == "navigation") {
+      }
+      if (info.entryType == "") {
+      }
+    });
+  });
+  // 观察的类型
+  observer.observe({
+    entryTypes: ["navigation", "resource"],
+    /**
+    frame：event-loop 时的每一帧
+    navigation：导航
+    resource：资源
+    mark: 打点，得到一个时间戳
+    measure：在两个点之间测量
+    paint：绘制
+    longtask(好像只有 chrome支持)：任何在浏览器中执行超过 50 ms 的任务，都是 long task
+   */
+  });
+};
+
+// 初始获取一次首屏加载数据
+const firstLoad = () => {
+  const t = window.performance.timing;
+  console.log("首次加载", t);
+  
 };
